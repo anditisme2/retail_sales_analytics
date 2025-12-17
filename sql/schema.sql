e7ce55a36fe712e3683152cf435fd1dd862e1b8a
Anushka Rana <anyway9350@gmail.com>
2025-12-17 20:58:23 +0530

    Commit Schema for the database

---
+++ sql/schema.sql
@@ -0,0 +1,37 @@
+CREATE TABLE IF NOT EXISTS customers (
+    customer_id TEXT PRIMARY KEY,
+    customer_unique_id TEXT,
+    customer_city TEXT,
+    customer_state TEXT
+);
+
+CREATE TABLE IF NOT EXISTS orders (
+    order_id TEXT PRIMARY KEY,
+    customer_id TEXT,
+    order_status TEXT,
+    order_purchase_timestamp TEXT,
+    FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+);
+
+CREATE TABLE IF NOT EXISTS products (
+    product_id TEXT PRIMARY KEY,
+    product_category_name TEXT
+);
+
+CREATE TABLE IF NOT EXISTS order_items (
+    order_item_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    order_id TEXT,
+    product_id TEXT,
+    price REAL,
+    freight_value REAL,
+    FOREIGN KEY (order_id) REFERENCES orders(order_id),
+    FOREIGN KEY (product_id) REFERENCES products(product_id)
+);
+
+CREATE TABLE IF NOT EXISTS payments (
+    payment_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    order_id TEXT,
+    payment_type TEXT,
+    payment_value REAL,
+    FOREIGN KEY (order_id) REFERENCES orders(order_id)
+);
